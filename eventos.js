c80434188e07307c57815135e9a1da8a9003e6f5
Joshu1610 <hernandezmorenojosemiguel@gmail.com>
2023-08-27 17:57:26 -0400

    Cambio del archivos eventos

--- eventos.js
+++ eventos.js
@@ -6,16 +6,16 @@ const list = document.querySelector('#list');
 
 
 const NUMBER_REGEX = /^((412)|(212)|(414)|(424)|(416)|(426))[0-9]{7}$/;
-const NAME_REGEX = /a/;
+const NAME_REGEX = /^[A-Z][a-z]{2,}/;
 
 
 // Funcion creada para validar que lo que esta dentro del input este bien puesto
-const validar = (input, verification) => {
+const validar = (input, verification, btn) => {
 
     if (nameValidation && numberValidation) {
-        formBtn.disabled = false;
+        btn.disabled = false;
     } else {
-        formBtn.disabled = true;
+        btn.disabled = true;
     }
 
     const message = input.parentElement.children[1];
@@ -43,15 +43,15 @@ let nameValidation = false;
 // Evento donde se llama al input del nombre 
 inputName.addEventListener('input', e => {
     // console.log(e.target.value);
-    nameValidation = inputName.value.length >= 3;
-    validar(inputName, nameValidation);
+    nameValidation = NAME_REGEX.test(inputName.value);
+    validar(inputName, nameValidation, formBtn);
 });
 
 // Evento donde se llama al input del numero 
 inputNumber.addEventListener('input', e => {
     // console.log(e.target.value);
     numberValidation = NUMBER_REGEX.test(inputNumber.value);
-    validar(inputNumber, numberValidation);
+    validar(inputNumber, numberValidation, formBtn);
 });
 
 
@@ -77,9 +77,18 @@ form.addEventListener('submit', e => {
     localStorage.setItem('listContacts', list.innerHTML);
 })
 
-
+const EditBtn = document.querySelector('.edit-icon')
 // Evento para elminar y editar
 list.addEventListener('click', e => {
+
+    // const successbtn = document.createElement('button');
+    // successbtn.innerHTML = `<button class="success-icon">
+    // <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    //     <path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75" />
+    // </svg>
+    // </button>
+    // `;
+
     if(e.target.closest('.delete-icon')){
 
         e.target.closest('.delete-icon').parentElement.remove();
@@ -91,22 +100,21 @@ list.addEventListener('click', e => {
 
         const inputEditName = e.target.closest('.edit-icon').parentElement.children[1];
         const inputEditNumber = e.target.closest('.edit-icon').parentElement.children[2];
-
-        // nameValidation = inputEditName.value.length >= 3;
-        // validar(inputEditName, nameValidation);
+        nameValidation = NAME_REGEX.test(inputName.value);
+        numberValidation = NUMBER_REGEX.test(inputNumber.value);
 
         if (inputEditName.hasAttribute('readonly')) {
 
             inputEditName.removeAttribute('readonly');
-            console.log(inputEditName.value);
+            
 
         } else {
 
             inputEditName.setAttribute('readonly',true);
             inputEditName.setAttribute('value', inputEditName.value);
-            console.log(inputEditName.value);
             localStorage.setItem('listContacts', list.innerHTML);
-
+            validar(inputEditName, nameValidation, EditBtn);
+            console.log(validar);
         }
 
         if (inputEditNumber.hasAttribute('readonly')) {
@@ -120,7 +128,6 @@ list.addEventListener('click', e => {
             inputEditNumber.setAttribute('value', inputEditNumber.value);
             console.log(inputEditNumber.value);
             localStorage.setItem('listContacts', list.innerHTML);
-
         }
     }
 })
